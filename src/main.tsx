fcd0ac829f848b86f5a5ea5e6844255671d63393
mohammad.safarzadeh <safarzadeh.ebrahim.m@gmail.com>
2023-09-11 21:23:27 +0330

    push all files

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import AuthProvider from './contexts/auth';
+import AuthProvider from './contexts/auth.tsx';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AuthProvider>
